4bfeccd14c9a264e6f1f3755eaf726affbf5d97d
Raquelvelis <raquel.veliscordero@go.minneapolis.edu>
2025-04-02 01:53:22 +0000

    Assigment 10 part 1

--- queries.sql
+++ queries.sql
@@ -37,3 +37,15 @@ VALUES
 ('Web Development', 4, 'Computer Science'),
 ('Data Structures', 4, 'Computer Science');
 GO
+
+SELECT 
+    s.FirstName + ' ' + s.LastName AS StudentName,
+    c.CourseName,
+    e.Grade
+FROM 
+    Students s
+    JOIN Enrollments e ON s.StudentID = e.StudentID
+    JOIN Courses c ON e.CourseID = c.CourseID
+ORDER BY 
+    s.LastName, s.FirstName;
+    GO
